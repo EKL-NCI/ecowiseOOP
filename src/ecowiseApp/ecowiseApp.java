77f9f5ed2496c94129cd4b0a32b1dad30b6ba1b0
Josh McGlynn <x21358616@student.ncirl.ie>
2023-12-11 22:59:11 +0000

    just one more comment i swear

--- src/ecowiseApp/ecowiseApp.java
+++ src/ecowiseApp/ecowiseApp.java
@@ -17,6 +17,7 @@ import javax.swing.JFrame;
 //Initially, we thought this had worked, but the next day we ran into issues with pushing and pulling files again, and so I had to do more research
 //I found that we actually needed to untrack the file paths that I wanted the .gitignore to ignore, or else GitHub would continue to push and pull them since it has already recognised them
 //Erin and I then played around with GitBash until we found the right commands to untrack the files that we didn't want being pushed or pulled
+//The command that worked for us was git rm --cached -r nbproject, and the same for the build folder. 
 //Since then, we had no issues with the repository, and things have been pushing and pulling seemlessly
 //This took way longer than it should have to figure out, multiple days of searching the internet for our specific issue, but we are all very glad to now have this knowledge for the future
 
